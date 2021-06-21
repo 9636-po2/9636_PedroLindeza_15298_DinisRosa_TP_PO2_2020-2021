d401a3a9fb7ad87476dfc2b639ea64baf036c86b
9636-po2 <9636@alunos.ipbeja.pt>
2021-06-21 16:07:46 +0100

    implementação do testeB

--- test/GameModelTest.java
+++ test/GameModelTest.java
@@ -3,8 +3,7 @@ import pt.ipbeja.estig.po2.boulderdash.gui.Board;
 import pt.ipbeja.estig.po2.boulderdash.model.GameModel;
 import pt.ipbeja.estig.po2.boulderdash.model.Rockford;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @author Pedro Lindeza, 9636 & Dinis Rosa, 15298
@@ -27,6 +26,17 @@ public class GameModelTest {
         assertTrue(validMove);
         assertEquals(row, rockford.getRow());
         assertEquals(col, rockford.getCol());
+    }
 
+    @Test
+    public void testeB(){
+        int row = 1;
+        int col = 1;
+
+        boolean validMove = this.model.moveRockford(row, col);
+
+        assertFalse(validMove);
+        assertEquals(1, rockford.getRow());
+        assertEquals(2, rockford.getCol());
     }
 }
